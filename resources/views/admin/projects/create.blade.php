8e4b10937e72baa7418f259fc3bc393d2389bf6d
MaTtiaa01 <mattia2001@live.it>
2023-01-16 17:00:49 +0100

    add type input in create form

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -10,25 +10,46 @@
         <form action="{{route('admin.projects.store')}}" method="post" class="card p-3" enctype="multipart/form-data">
             @csrf
 
+            <!-- choose a title -->
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="insert a name" aria-describedby="helpId">
                 <small id="helpId" class="text-muted">insert a project name</small>
             </div>
+
+            <!-- choose a cover image -->
             <div class="mb-3">
                 <label for="cover_img" class="form-label">cover_img</label>
                 <input type="file" name="cover_img" id="cover_img" class="form-control @error('cover_img') is-invalid @enderror" placeholder="insert a name" aria-describedby="helpId">
                 <small id="helpId" class="text-muted">insert a cover image</small>
             </div>
+
+            <!-- select type -->
+            <div class="mb-3">
+                <label for="type_id" class="form-label">Type</label>
+                <select class="form-select form-select-md @error('type_id') 'is-invalid' @enderror " name="type_id" id="type_id">
+                    <option selected>Select one</option>
+
+                    @foreach($types as $type)
+                    <option value="{{$type->id}}" {{old('type_id') == $type->id ? 'selected' : ''}}>{{$type->name}}</option>
+                    @endforeach
+
+                </select>
+            </div>
+
+
+            <!-- write a description -->
             <div class="mb-3">
                 <label for="description" class="form-label">description</label>
                 <input type="text" name="description" id="description" class="form-control @error('description') is-invalid @enderror" placeholder="insert a description" aria-describedby="helpId">
                 <small id="helpId" class="text-muted">insert a project description</small>
             </div>
+
+            <!-- write project language -->
             <div class="mb-3">
                 <label for="language" class="form-label">language</label>
                 <input type="text" name="language" id="language" class="form-control @error('language') is-invalid @enderror" placeholder="insert a language url" aria-describedby="helpId">
-                <small id="helpId" class="text-muted">insert a project language url</small>
+                <small id="helpId" class="text-muted">insert a project language </small>
             </div>
 
             <button type="submit" class="btn btn-primary">Add</button>
